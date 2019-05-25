f1aeff574127350a8c5bb6738e251d683bbc2511
ayhankuru <ayhankuru@yandex.com.tr>
2019-05-25 21:10:47 +0300

    Updated

--- src/index.js
+++ src/index.js
@@ -1,64 +1,87 @@
-const puppet = require('puppeteer-core')
-//const chrome = require('chrome-aws-lambda')
-const { Request, NetworkFilter } = require('@cliqz/adblocker')
+const puppeteer = require('puppeteer');
+const fetch =  require('isomorphic-unfetch')
+const { parse } =  require('tldts')
+const { makeRequest, fastHash, FiltersEngine, fetchLists, fetchResources } = require('@cliqz/adblocker')
 
-const filter = NetworkFilter.parse('||githubassets.com/assets/dashboard-bootstrap-80793253.js$script');
-
-async function getOptions(isDev) {
-  let options = {}
-  if (isDev) {
-    options = {
-      args: [],
-      executablePath:
-        'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
-      headless: false
+function loadAdblocker() {
+  console.log('Fetching resources...');
+  return Promise.all([fetchLists(), fetchResources()]).then(([responses, resources]) => {
+    console.log('Initialize adblocker...');
+    const deduplicatedLines = new Set();
+    for (let i = 0; i < responses.length; i += 1) {
+      const lines = responses[i].split(/\n/g);
+      for (let j = 0; j < lines.length; j += 1) {
+        deduplicatedLines.add(lines[j]);
+      }
     }
-  } else {
-    options = {
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless
-    }
-  }
-  return options
-}
+    const deduplicatedFilters = [...deduplicatedLines].join('\n');
 
-let _page = null
-let _browser = null
+    let t0 = Date.now();
+    const engine = FiltersEngine.parse(deduplicatedFilters);
+    let total = Date.now() - t0;
+    console.log('parsing filters', total);
 
-async function getPage() {
-  const options = await getOptions(true)
+    t0 = Date.now();
+    engine.updateResources(resources, '' + fastHash(resources));
+    total = Date.now() - t0;
+    console.log('parsing resources', total);
 
-  if (!_browser) {
-    _browser = await puppet.launch(options)
-  }
+    t0 = Date.now();
+    const serialized = engine.serialize();
+    total = Date.now() - t0;
+    console.log('serialization', total);
+    console.log('size', serialized.byteLength);
 
-  _page = await _browser.newPage()
+    t0 = Date.now();
+    const deserialized = FiltersEngine.deserialize(serialized);
+    total = Date.now() - t0;
+    console.log('deserialization', total);
 
-  return _page
-}
-
-async function getScreenshot(url) {
-  const page = await getPage()
-  
-  await page.setRequestInterception(true);
-
-  page.on('request', interceptedRequest => {
-
-      console.log(filter.match(interceptedRequest))
-      console.log(interceptedRequest.url())
-      interceptedRequest.continue();
+    return deserialized;
   });
-
-  await page.goto(url)
-
-  //await page.close()
-  //return text
 }
 
 
 (async () => {
 
-  await getScreenshot('https://www.github.com')
+
+  const engine = await loadAdblocker()
+
+  const browser = await puppeteer.launch({ 
+        headless: false, 
+        devtools: false
+    });
+  
+  const page = await browser.newPage();
+    
+  await page.setRequestInterception(true);
+  
+ 
+  page.on('request', interceptedRequest => {
+
+      const { redirect, match } = engine.match(makeRequest(
+          {
+            sourceUrl: interceptedRequest.url(),
+            type: interceptedRequest.resourceType(),
+            url: interceptedRequest.url()
+          },
+          parse
+        ));
+
+      if(redirect){
+        console.log('ABORT REDIRECT')
+        interceptedRequest.abort()
+      } else if(match){
+        console.log('ABORT MATCH')
+        interceptedRequest.abort()
+      } else {
+        interceptedRequest.continue();
+      }
+
+      
+  });
+
+
+   await page.goto('https://www.nytimes.com/')
 
 })()
